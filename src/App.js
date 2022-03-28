dd0a68f3366a5ec67fe5505e48eaadf5cf957794
RadzioN <radeknapora2003@gmail.com>
2022-03-28 20:56:13 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
-  const [value, onChange] = useState(new Date());
+  const [value, onChangeDate] = useState(new Date());
 
-  const events = [new Date(2022, 3, 2), new Date(2022, 3, 5)]
+  const events = [
+    new Date(2022, 3, 2),
+    new Date(2022, 3, 5)
+  ];
 
   return (
     <Container fluid>
       <Row>
         <Col>
-          <Calendar onChange={onChange} value={value} tileClassName={({ date, view }) => view === 'month' && events.toLocaleString().includes(date.toLocaleString()) ? 'event' : null}/>
-        </Col>
-        <Col md lg="4" className='eventSide'>
-          <Container fluid className='eventBar'>
-            <h2>Wydarzenia</h2>
-          </Container>
-          <Button variant="light" className='addEvent'>Dodaj</Button>
+          <Calendar onChange={onChangeDate} value={value} tileClassName={({ date, view }) => view === 'month' && events.toLocaleString().includes(date.toLocaleString()) ? 'event' : null}/>
         </Col>
       </Row>
     </Container>
