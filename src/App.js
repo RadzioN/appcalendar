25cdfbd479f20c87d33cff1ed0bdf4dc9cef7306
RadzioN <radeknapora2003@gmail.com>
2022-03-28 21:03:48 +0200

    Static

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Calendar from 'react-calendar';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
-  const [value, onChangeDate] = useState(new Date());
-
   const events = [
     new Date(2022, 3, 2),
     new Date(2022, 3, 5)
@@ -15,7 +13,7 @@ function App() {
     <Container fluid>
       <Row>
         <Col>
-          <Calendar onChange={onChangeDate} value={value} tileClassName={({ date, view }) => view === 'month' && events.toLocaleString().includes(date.toLocaleString()) ? 'event' : null}/>
+          <Calendar tileClassName={({ date, view }) => view === 'month' && events.toLocaleString().includes(date.toLocaleString()) ? 'event' : null}/>
         </Col>
       </Row>
     </Container>
