b766fb9407e5c85e57717c99a080890d865d0058
RadzioN <radeknapora2003@gmail.com>
2022-04-03 19:31:36 +0200

    Init ClearCalendar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import './App.css';
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import { Container, Row, Col } from 'react-bootstrap';
+
+function CalendarDays(props) {
+
+  let firstDay = (new Date(props.year, props.month)).getDate();
+
+  function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+  }
+
+  let date = 1;
+
+  var [calendarRows] = [6][7];
+
+  for(let i = 0; i < 6; i++) {
+    for(let j = 0; j < 7; j++) {
+      if(i === 0 && j < firstDay) {
+        calendarRows[i][j] = "";
+      } else if(date > daysInMonth(props.month, props.year)) {
+        break;
+      } else {
+        calendarRows[i][j] = date;
+        date++;
+      }
+    }
+  }
+
+  console.log(calendarRows);
+
+  return(
+    <div>
+      
+    </div>
+  );
+}
 
 function App() {
-  const [value, onChangeDate] = useState(new Date());
-
-  const events = [
-    new Date(2022, 3, 2),
-    new Date(2022, 3, 5)
-  ];
-
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <Calendar onChange={onChangeDate} value={value} tileClassName={({ date, view }) => view === 'month' && events.toLocaleString().includes(date.toLocaleString()) ? 'event' : null}/>
-        </Col>
-      </Row>
-    </Container>
+    <div className="App">
+      <h1>Kalendarz</h1>
+      <CalendarDays year="2022" month="3"/>
+    </div>
   );
 }
 
